3f71e90cecf40e15499b5e7ffe574deef4b84636
Armani <Armantakhanit6@gmail.com>
2024-10-24 09:15:37 +0200

    Koden

---
+++ halloween3.py
@@ -0,0 +1,9 @@
+karaktar = input("What's your favorite animal? ")
+if karaktar == "monkey":
+    print("Here's a banana")
+elif karaktar == "hippo":
+    print("Here's a watermenlon")
+elif karaktar == "horse":
+    print("Here's an apple")
+else:
+    print("What a cute animal you chose")
